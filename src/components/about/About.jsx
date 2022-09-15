6f6921bf89afe9e45f2fa65708e0f8d50ae35352
agrey tbamanya <agreytibamanya04@gmail.com>
2022-09-15 23:57:31 +0300

    file added

---
+++ src/components/about/About.jsx
@@ -0,0 +1,45 @@
+import "./about.css";
+
+import me from "../../img/tiba.png"
+
+const About = () => {
+  return (
+    <div className="a">
+      <div className="a-left">
+        <div className="a-card bg"></div>
+        <div className="a-card">
+          <img
+            src={me}
+            alt=""
+            className="a-img"
+          />
+        </div>
+      </div>
+      <div className="a-right">
+        <h1 className="a-title">About Me</h1>
+        <p className="a-sub">
+          It is a long established fact that a reader will be distracted by the
+          readable content.
+        </p>
+        <p className="a-desc">
+          Fullstack developer dealing with website,web app devepoler, E-mail template && No SQL Database.
+          <h4>SKILLS & EXPERTISE</h4>
+          <ul>
+            <li>responsive website development</li>
+            <li>update existing website</li>
+            <li>E-commerce</li>
+            <li>web App development</li>
+            <li>E-mail templates</li>
+            <li>mobile application development</li>
+          </ul>
+        </p>
+        <div className="a-award">
+         
+         
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+  {
+    id: 1,
+    img: "./img/tiba.png",
+    link: "http://lama.dev",
+  },
+  {
+    id: 2,
+    img: "./img/tiba.png",
+    link: "http://lama.dev",
+  },
+  {
+    id: 3,
+    img: "./img/tiba.png",
+    link: "http://lama.dev",
+  },
+  {
+    id: 4,
+    img: "./img/tiba.png",
+    link: "http://lama.dev",
+  },
+  {
+    id: 5,
+    img: "",
+    link: "http://lama.dev",
+  },
+  {
+    id: 6,
+    img: "",
+    link: "http://lama.dev",
+  },
+];
